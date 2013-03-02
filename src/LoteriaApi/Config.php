784745a9703b9943bcdece272c73c62bd7568d99
Marcus Sa <marcusesa@gmail.com>
2013-03-02 15:52:15 -0300

    Implementando metodos na classe Config.

--- src/LoteriaApi/Config.php
+++ src/LoteriaApi/Config.php
@@ -4,9 +4,27 @@ namespace LoteriaApi;
 
 class Config {
     private $apiPath;
+    private $directory;
+    private $filename;
+    private $ext;
 
     public function setApiPath($apiPath) {
         $this->apiPath = $apiPath;
+        return $this;
+    }
+    
+    public function setDirectory($directory) {
+        $this->directory = $directory;
+        return $this;
+    }
+    
+    public function setFileName($filename) {
+        $this->filename = $filename;
+        return $this;
+    }
+    
+    public function setExt($ext) {
+        $this->ext = $ext;
         return $this;
     }
 }
