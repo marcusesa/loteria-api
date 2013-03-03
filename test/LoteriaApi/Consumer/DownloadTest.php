54bac0a901e70a306574247ed2e09a1e95e20d86
Marcus Sa <marcusesa@gmail.com>
2013-03-03 20:48:41 -0300

    Testando os metodos setConfig e run da classe Download.

--- test/LoteriaApi/Consumer/DownloadTest.php
+++ test/LoteriaApi/Consumer/DownloadTest.php
@@ -22,4 +22,29 @@ class DownloadTest extends \PHPUnit_Framework_TestCase {
         $instance = $this->download->setComponent(new Downloader);
         $this->assertInstanceOf('LoteriaApi\Consumer\Download', $instance);
     }
+    
+    public function testSetConfigShouldReturnInstanceOfDownload() {
+        $instance = $this->download->setConfig([]);
+        $this->assertInstanceOf('LoteriaApi\Consumer\Download', $instance);        
+    }
+    
+    public function testRunShouldDownloadFiles() {
+        $config = $this->getMock('\LoteriaApi\Config', ['getData']);
+        $config->expects($this->any())
+            ->method('getData')
+            ->will($this->returnValue([
+            'megasena' => [
+                'name' => 'Mega-Sena',
+                'url' => 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip',
+                'zip' => 'megasena.zip'
+            ]
+        ]));
+        
+        $this->download
+            ->setComponent(new Downloader)
+            ->setConfig($config->getData())
+            ->run();
+        
+        $this->assertFileExists(API_PATH.DS.'var'.DS.'zip'.DS.'megasena.zip');
+    }
 }
