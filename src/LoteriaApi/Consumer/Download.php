690f4ff0008c186f8077ec9fec97c413c8f4a9fe
Marcus Sa <marcusesa@gmail.com>
2013-03-03 20:48:18 -0300

    Implementando os metodos setConfig e run da classe Download.

--- src/LoteriaApi/Consumer/Download.php
+++ src/LoteriaApi/Consumer/Download.php
@@ -6,9 +6,30 @@ use Kodify\DownloaderBundle\Service\Downloader;
 
 class Download {
     private $component;
+    private $config;
+    private $pathFiles;
+    
+    public function __construct() {
+        $this->pathFiles = API_PATH.DS.'var'.DS.'zip'.DS;
+    }
 
     public function setComponent(Downloader $component){
         $this->component = $component;
         return $this;
+    }
+    
+    public function setConfig(array $config) {
+        $this->config = $config;
+        return $this;
+    }
+    
+    public function run() {
+        foreach ($this->config as $concurso) {
+            $this->component->downloadFile(
+                    $concurso['url'], 
+                    $this->pathFiles, 
+                    $concurso['zip']
+            );
+        }
     }
 }
