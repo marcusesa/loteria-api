22cfdc0f0e37dc5dfdfed2552e449b6f67cdc0ff
Marcus Sa <marcusesa@gmail.com>
2013-11-15 22:31:03 -0200

    Adicionando a Classe Reader.

---
+++ src/LoteriaApi/Consumer/Reader.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LoteriaApi\Consumer;
+
+use \DOMDocument;
+
+class Reader {
+    private $datasource;
+    private $paths;    
+
+    public function setDataSource($datasource){
+        $this->datasource = $datasource;
+        return $this;
+    }
+    
+    public function setPathsStorage($paths){
+        $this->paths = $paths;
+        return $this;
+    }
+    
+    public function getData(){
+        $data = [];
+        foreach ($this->datasource as $concursoName => $concursoData) {
+            $file = $this->paths['path']['ext'].$concursoData['html'];
+            $doc = DOMDocument::loadHTMLFile($file);
+            $data[$concursoName] = (new $concursoData['reader'])
+                ->setDOMDocument($doc)
+                ->getData();
+        }
+        return $data;
+    }
+}
