86c117e211d81eee70f4c2f85c0876e56cc6b6c4
Marcus Sá <marcusesa@gmail.com>
2014-01-24 13:57:32 -0200

    [Lotofacil] Adicionando ao WriterTest a escrita de uma nova loteria.

--- test/LoteriaApi/Consumer/WriterTest.php
+++ test/LoteriaApi/Consumer/WriterTest.php
@@ -94,4 +94,86 @@ class WriterTest extends \PHPUnit_Framework_TestCase {
         unlink($file);
     }
 
+    
+    public function testRunShouldCreateXmlOfLotofacil() {
+        $datasources = [
+            'lotofacil' => [
+                'xml' => 'lotofacil.xml'
+            ]
+        ];
+
+        $paths = [
+            'path' => [
+                'xml' => API_PATH . 'var' . DS . '_test' . DS . 'xml' . DS
+            ]
+        ];
+
+        $data = [
+            'lotofacil' => [
+                // primeiro concurso
+                1 => [
+                    'data' => '29/09/2003',
+                    'dezenas' => [
+                        0 => '18',
+                        1 => '20',
+                        2 => '25',
+                        3 => '23',
+                        4 => '10',
+                        5 => '11',
+                        6 => '24',
+                        7 => '14',
+                        8 => '06',
+                        9 => '02',
+                        10 => '13',
+                        11 => '09',
+                        12 => '05',
+                        13 => '16',
+                        14 => '03',
+                    ],
+                    'arrecadacao' => '0,00',
+                    'total_ganhadores' => '5',
+                    'acumulado' => 'NÃO',
+                    'valor_acumulado' => '0,00'
+                ],
+                // ultimo concurso
+                992 => [
+                    'data' => '09/12/2013',
+                    'dezenas' => [
+                        0 => '05',
+                        1 => '17',
+                        2 => '08',
+                        3 => '24',
+                        4 => '20',
+                        5 => '15',
+                        6 => '11',
+                        7 => '13',
+                        8 => '09',
+                        9 => '03',
+                        10 => '19',
+                        11 => '01',
+                        12 => '07',
+                        13 => '04',
+                        14 => '10',
+                    ],
+                    'arrecadacao' => '19.036.316,25',
+                    'total_ganhadores' => '3',
+                    'acumulado' => 'NÃO',
+                    'valor_acumulado' => '0,00'
+                ],
+            ]
+        ];
+        
+        $this->writer
+            ->setDataSource($datasources)
+            ->setLocalStorage($paths['path']['xml'])    
+            ->setData($data)
+            ->run();
+        
+        $file = $paths['path']['xml'].$datasources['lotofacil']['xml'];
+        $this->assertFileExists($file);
+        $fileTest = $paths['path']['xml'].'test_lotofacil.xml';
+        $this->assertFileEquals($fileTest, $file);
+        unlink($file);
+    }    
+
 }
