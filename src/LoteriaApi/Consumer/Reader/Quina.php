0fb0370fe4d585ad96f933f058291ae7efe12864
Marcus Sa <marcusesa@gmail.com>
2014-01-26 00:13:59 -0200

    [Quina/Lotomania] Atualizando o ReaderTest.

---
+++ src/LoteriaApi/Consumer/Reader/Quina.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LoteriaApi\Consumer\Reader;
+
+use \DOMDocument;
+
+class Quina implements IReader {
+    private $domdocument;
+    
+    public function setDOMDocument(DOMDocument $domdocument){
+        $this->domdocument = $domdocument;
+        return $this;
+    }
+    
+    public function getData(){
+        $data = [];
+        
+        $table = $this->domdocument->getElementsByTagName('table')->item(0);
+        $trs = $table->getElementsByTagName('tr');
+
+        for ($concursoHtml = 1; $concursoHtml < $trs->length; $concursoHtml++) {
+            $tds = $trs->item($concursoHtml)->getElementsByTagName('td');
+            
+            $nrconcurso = $tds->item(0)->nodeValue;
+
+            $data[$nrconcurso] = [
+                'data' => $tds->item(1)->nodeValue,
+                'dezenas' => [
+                    0 => $tds->item(2)->nodeValue,
+                    1 => $tds->item(3)->nodeValue,
+                    2 => $tds->item(4)->nodeValue,
+                    3 => $tds->item(5)->nodeValue,
+                    4 => $tds->item(6)->nodeValue,
+                ],
+                'arrecadacao' => $tds->item(7)->nodeValue,
+                'total_ganhadores' => $tds->item(8)->nodeValue,
+                'acumulado' => $tds->item(14)->nodeValue,
+                'valor_acumulado' => $tds->item(15)->nodeValue,
+            ];
+        }            
+        return $data;
+    }
+}
--- test/LoteriaApi/Consumer/ReaderTest.php
+++ test/LoteriaApi/Consumer/ReaderTest.php
@@ -165,4 +165,152 @@ class ReaderTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals($dataExpect['lotofacil'][1],     $data['lotofacil'][1]);
         $this->assertEquals($dataExpect['lotofacil'][992],  $data['lotofacil'][992]);
     }
+
+    public function testGetDataShouldReturnArrayExpectOfQuina(){
+        $dataExpect = [
+            'quina' => [
+                // primeiro concurso
+                1 => [
+                    'data' => '13/03/1994',
+                    'dezenas' => [
+                        0 => '25',
+                        1 => '45',
+                        2 => '60',
+                        3 => '76',
+                        4 => '79',
+                    ],
+                    'arrecadacao' => '0,00',
+                    'total_ganhadores' => '3',
+                    'acumulado' => 'NÃO',
+                    'valor_acumulado' => '0,00'
+                ],
+                // ultimo concurso
+                3399 => [
+                    'data' => '24/01/2014',
+                    'dezenas' => [
+                        0 => '55',
+                        1 => '75',
+                        2 => '42',
+                        3 => '23',
+                        4 => '64',
+                    ],
+                    'arrecadacao' => '5.909.673,75',
+                    'total_ganhadores' => '0',
+                    'acumulado' => 'SIM',
+                    'valor_acumulado' => '1.170.673,89'
+                ],
+            ]
+        ];
+        
+        $datasources = [
+            'quina' => [
+                'html'    => "test_D_QUINA.HTM",
+                'reader'  => "LoteriaApi\Consumer\Reader\Quina",
+            ]
+        ];
+        
+        $paths = [
+            'path' => [
+                'ext' => API_PATH . 'var' . DS . '_test' . DS . 'ext' . DS,
+            ]
+        ];
+        
+        $data = $this->reader
+            ->setDataSource($datasources)
+            ->setPathsStorage($paths)
+            ->getData();
+        
+        $this->assertArrayHasKey('quina', $dataExpect);
+        $this->assertEquals($dataExpect['quina'][1],     $data['quina'][1]);
+        $this->assertEquals($dataExpect['quina'][3399],  $data['quina'][3399]);
+    }
+
+    public function testGetDataShouldReturnArrayExpectOfLotomania(){
+        $dataExpect = [
+            'lotomania' => [
+                // primeiro concurso
+                1 => [
+                    'data' => '02/10/1999',
+                    'dezenas' => [
+                        0 => '16',
+                        1 => '11',
+                        2 => '88',
+                        3 => '32',
+                        4 => '25',
+                        5 => '00',
+                        6 => '70',
+                        7 => '78',
+                        8 => '73',
+                        9 => '61',
+                        10 => '90',
+                        11 => '89',
+                        12 => '46',
+                        13 => '95',
+                        14 => '06',
+                        15 => '33',
+                        16 => '34',
+                        17 => '21',
+                        18 => '14',
+                        19 => '22',
+                    ],
+                    'arrecadacao' => '0,00',
+                    'total_ganhadores' => '0',
+                    'acumulado' => 'SIM',
+                    'valor_acumulado' => '178.120,31'
+                ],
+                // ultimo concurso
+                1420 => [
+                    'data' => '22/01/2014',
+                    'dezenas' => [
+                        0 => '26',
+                        1 => '67',
+                        2 => '12',
+                        3 => '60',
+                        4 => '77',
+                        5 => '68',
+                        6 => '39',
+                        7 => '34',
+                        8 => '79',
+                        9 => '59',
+                        10 => '35',
+                        11 => '93',
+                        12 => '92',
+                        13 => '73',
+                        14 => '23',
+                        15 => '70',
+                        16 => '27',
+                        17 => '96',
+                        18 => '17',
+                        19 => '36',
+                    ],
+                    'arrecadacao' => '5.231.545,50',
+                    'total_ganhadores' => '0',
+                    'acumulado' => 'SIM',
+                    'valor_acumulado' => '2.397.228,70'
+                ],
+            ]
+        ];
+        
+        $datasources = [
+            'lotomania' => [
+                'html' => 'test_D_LOTMAN.HTM',
+                'reader' => 'LoteriaApi\Consumer\Reader\Lotomania',
+            ]
+        ];
+        
+        $paths = [
+            'path' => [
+                'ext' => API_PATH . 'var' . DS . '_test' . DS . 'ext' . DS,
+            ]
+        ];
+        
+        $data = $this->reader
+            ->setDataSource($datasources)
+            ->setPathsStorage($paths)
+            ->getData();
+        
+        $this->assertArrayHasKey('lotomania', $dataExpect);
+        $this->assertEquals($dataExpect['lotomania'][1],     $data['lotomania'][1]);
+        $this->assertEquals($dataExpect['lotomania'][1420],  $data['lotomania'][1420]);
+    }
 }
