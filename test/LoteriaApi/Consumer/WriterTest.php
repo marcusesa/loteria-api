74508073bee4dae0eb13f6cb446b471851be9c41
Marcus Sá <marcusesa@gmail.com>
2014-01-27 10:17:19 -0200

    [Quina/Lotomania] Atualizando o WriterTest.

--- test/LoteriaApi/Consumer/WriterTest.php
+++ test/LoteriaApi/Consumer/WriterTest.php
@@ -176,4 +176,155 @@ class WriterTest extends \PHPUnit_Framework_TestCase {
         unlink($file);
     }    
 
+    public function testRunShouldCreateXmlOfLotomania() {
+        $datasources = [
+            'lotomania' => [
+                'xml' => 'lotomania.xml'
+            ]
+        ];
+
+        $paths = [
+            'path' => [
+                'xml' => API_PATH . 'var' . DS . '_test' . DS . 'xml' . DS
+            ]
+        ];
+
+        $data = [
+            'lotomania' => [
+                // primeiro concurso
+                1 => [
+                    'data' => '02/10/1999',
+                    'dezenas' => [
+                        0 => '16',
+                        1 => '11',
+                        2 => '88',
+                        3 => '32',
+                        4 => '25',
+                        5 => '00',
+                        6 => '70',
+                        7 => '78',
+                        8 => '73',
+                        9 => '61',
+                        10 => '90',
+                        11 => '89',
+                        12 => '46',
+                        13 => '95',
+                        14 => '06',
+                        15 => '33',
+                        16 => '34',
+                        17 => '21',
+                        18 => '14',
+                        19 => '22',
+                    ],
+                    'arrecadacao' => '0,00',
+                    'total_ganhadores' => '0',
+                    'acumulado' => 'SIM',
+                    'valor_acumulado' => '178.120,31'
+                ],
+                // ultimo concurso
+                1420 => [
+                    'data' => '22/01/2014',
+                    'dezenas' => [
+                        0 => '26',
+                        1 => '67',
+                        2 => '12',
+                        3 => '60',
+                        4 => '77',
+                        5 => '68',
+                        6 => '39',
+                        7 => '34',
+                        8 => '79',
+                        9 => '59',
+                        10 => '35',
+                        11 => '93',
+                        12 => '92',
+                        13 => '73',
+                        14 => '23',
+                        15 => '70',
+                        16 => '27',
+                        17 => '96',
+                        18 => '17',
+                        19 => '36',
+                    ],
+                    'arrecadacao' => '5.231.545,50',
+                    'total_ganhadores' => '0',
+                    'acumulado' => 'SIM',
+                    'valor_acumulado' => '2.397.228,70'
+                ],
+            ]
+        ];
+
+        $this->writer
+            ->setDataSource($datasources)
+            ->setLocalStorage($paths['path']['xml'])    
+            ->setData($data)
+            ->run();
+        
+        $file = $paths['path']['xml'].$datasources['lotomania']['xml'];
+        $this->assertFileExists($file);
+        $fileTest = $paths['path']['xml'].'test_lotomania.xml';
+        $this->assertFileEquals($fileTest, $file);
+        unlink($file);
+    }
+
+    public function testRunShouldCreateXmlOfQuina() {
+        $datasources = [
+            'quina' => [
+                'xml' => 'quina.xml'
+            ]
+        ];
+
+        $paths = [
+            'path' => [
+                'xml' => API_PATH . 'var' . DS . '_test' . DS . 'xml' . DS
+            ]
+        ];
+
+        $data = [
+            'quina' => [
+                // primeiro concurso
+                1 => [
+                    'data' => '13/03/1994',
+                    'dezenas' => [
+                        0 => '25',
+                        1 => '45',
+                        2 => '60',
+                        3 => '76',
+                        4 => '79',
+                    ],
+                    'arrecadacao' => '0,00',
+                    'total_ganhadores' => '3',
+                    'acumulado' => 'NÃO',
+                    'valor_acumulado' => '0,00'
+                ],
+                // ultimo concurso
+                3399 => [
+                    'data' => '24/01/2014',
+                    'dezenas' => [
+                        0 => '55',
+                        1 => '75',
+                        2 => '42',
+                        3 => '23',
+                        4 => '64',
+                    ],
+                    'arrecadacao' => '5.909.673,75',
+                    'total_ganhadores' => '0',
+                    'acumulado' => 'SIM',
+                    'valor_acumulado' => '1.170.673,89'
+                ],
+            ]
+        ];
+
+        $this->writer
+            ->setDataSource($datasources)
+            ->setLocalStorage($paths['path']['xml'])    
+            ->setData($data)
+            ->run();
+        
+        $file = $paths['path']['xml'].$datasources['quina']['xml'];
+        $this->assertFileExists($file);
+        $fileTest = $paths['path']['xml'].'test_quina.xml';
+        $this->assertFileEquals($fileTest, $file);
+        unlink($file);
+    }
 }
