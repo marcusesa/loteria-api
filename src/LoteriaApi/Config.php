aa4a193a63f3c7e1e52836e98c6c4e4925a74a1f
Marcus Sá <marcusesa@gmail.com>
2014-02-05 14:44:56 -0200

    Ajustes CodeStyle class Config.

--- src/LoteriaApi/Config.php
+++ src/LoteriaApi/Config.php
@@ -2,39 +2,47 @@
 
 namespace LoteriaApi;
 
-class Config {
+class Config
+{
     private $apiPath;
     private $directory;
     private $filename;
     private $ext;
 
-    public function setApiPath($apiPath) {
+    public function setApiPath($apiPath)
+    {
         $this->apiPath = $apiPath;
         return $this;
     }
     
-    public function setDirectory($directory) {
+    public function setDirectory($directory)
+    {
         $this->directory = $directory;
         return $this;
     }
     
-    public function setFileName($filename) {
+    public function setFileName($filename)
+    {
         $this->filename = $filename;
         return $this;
     }
-    
-    public function setExt($ext) {
+
+    public function setExt($ext)
+    {
         $this->ext = $ext;
         return $this;
     }
-    
-    public function getData(){
-        $file = $this->apiPath. 
-                $this->directory. DS .
-                $this->filename. '.' .  $this->ext;
-        
-        if(!is_file($file))
-            throw new \Exception ('File does not exist');
+
+    public function getData()
+    {
+        $file = $this->apiPath .
+            $this->directory . DS .
+            $this->filename . '.' .
+            $this->ext;
+
+        if (!is_file($file)) {
+            throw new \Exception('File does not exist');
+        }
 
         return parse_ini_file($file, true);
     }
