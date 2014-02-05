9787862d08e36a1abf8098362bfead92b2f3a519
Marcus Sá <marcusesa@gmail.com>
2014-02-05 14:45:40 -0200

    Ajustes CodeStyle class Download.

--- src/LoteriaApi/Consumer/Download.php
+++ src/LoteriaApi/Consumer/Download.php
@@ -4,32 +4,37 @@ namespace LoteriaApi\Consumer;
 
 use Kodify\DownloaderBundle\Service\Downloader;
 
-class Download {
+class Download
+{
     private $component;
     private $datasource;
     private $localstorage;
     
-    public function setComponent(Downloader $component){
+    public function setComponent(Downloader $component)
+    {
         $this->component = $component;
         return $this;
     }
     
-    public function setDataSource(array $datasource) {
+    public function setDataSource(array $datasource)
+    {
         $this->datasource = $datasource;
         return $this;
     }
     
-    public function setLocalStorage($localstorage) {
+    public function setLocalStorage($localstorage)
+    {
         $this->localstorage = $localstorage;
         return $this;
     }
 
-    public function run() {
+    public function run()
+    {
         foreach ($this->datasource as $concurso) {
             $this->component->downloadFile(
-                    $concurso['url'], 
-                    $this->localstorage, 
-                    $concurso['zip']
+                $concurso['url'],
+                $this->localstorage,
+                $concurso['zip']
             );
         }
     }
