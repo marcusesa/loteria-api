c0b9d3700483eec1f1b599c69c72580ba5c6e757
Marcus Sá <marcusesa@gmail.com>
2014-02-14 11:36:22 -0200

    Classe XmlQuina.

---
+++ src/LoteriaApi/Provider/Reader/XmlQuina.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LoteriaApi\Provider\Reader;
+
+class XmlQuina extends AbstractXmlLoteria
+{
+    protected function putFileName(){
+        $filename = $this->configDatasource->getData()['quina']['xml'];
+        $path = $this->configPath->getData()['path']['xml'];
+        $this->filename = $path . $filename;    
+    }
+}
