967f9e34b6f0b4d39cce971f5007d2090bc21c26
Marcus Sá <marcusesa@gmail.com>
2014-02-24 14:47:51 -0300

    Alterando o app pois agora o codigo esta divido no controler, services e providers.

--- web/app.php
+++ web/app.php
@@ -1,48 +1,23 @@
 <?php
 
 use Silex\Application;
-use LoteriaApi\Provider\Factory;
-use LoteriaApi\Config;
+use LoteriaApi\Provider\Services\FactoryServiceProvider;
+use LoteriaApi\Provider\Routes\IndexControllerProvider;
+use LoteriaApi\Provider\Services\ConfigServiceProvider;
 
 $app = new Application();
 
-$app['config.api_path'] = API_PATH;
-$app['config.directory'] = 'etc';
-$app['config.ext'] = 'ini';
+$app->register(new ConfigServiceProvider(), [
+    'config.api_path'      => API_PATH,
+    'config.directory'     => 'etc',
+    'config.ext'           => 'ini',
+]);
 
-$app['config'] = function ($app) {
-    return (new Config)
-        ->setApiPath($app['config.api_path'])
-        ->setDirectory($app['config.directory'])
-        ->setExt($app['config.ext']);
-};
+$app->register(new FactoryServiceProvider, [
+    'config.datasources' => $app['config.datasources'],
+    'config.path'        => $app['config.path'],
+]);
 
-$app['config.datasources'] = function ($app) {
-    return $app['config']
-        ->setFileName('datasource');
-};
-
-$app['config.path'] = function ($app) {
-    return $app['config']
-        ->setFileName('path');
-};
-
-$app['factory'] = $app->share(function ($app) {
-    return new Factory($app['config.path'], $app['config.datasources']);
-});
-
-$app->get('/', function() use($app) {
-    $loteria = $app['request']->get('loteria');
-    $concurso = $app['request']->get('concurso');
-
-    try {
-        $loteria = $app['factory']
-            ->getLoteria($loteria)
-            ->findByConcurso($concurso);
-        return $app->json($loteria, 200);
-    } catch(\Exception $e) {
-        return $app->abort(400);
-    }
-});
+$app->mount('', new IndexControllerProvider());
 
 return $app;
