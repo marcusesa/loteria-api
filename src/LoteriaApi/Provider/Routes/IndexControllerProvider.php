619ddf84314b5199202489198487217acbfbc054
Marcus Sá <marcusesa@gmail.com>
2014-02-24 14:48:16 -0300

    Adicionando o controller index.

---
+++ src/LoteriaApi/Provider/Routes/IndexControllerProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LoteriaApi\Provider\Routes;
+
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+
+class IndexControllerProvider implements ControllerProviderInterface
+{
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/', function () use ($app) {
+            $loteria = $app['request']->get('loteria');
+            $concurso = $app['request']->get('concurso');
+
+            try {
+                $loteria = $app['factory']
+                    ->getLoteria($loteria)
+                    ->findByConcurso($concurso);
+                return $app->json($loteria, 200);
+            } catch (\Exception $e) {
+                return $app->abort(400);
+            }
+        });
+
+        return $controllers;
+    }
+}
